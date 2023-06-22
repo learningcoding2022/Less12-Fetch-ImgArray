7b43b69e956f37d3570bc67672b66f3d43e60a3a
molly_b <brownmolly109@gmail.com>
2023-06-22 12:46:15 -0400

    fetched the API data for images

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
+const getImage = async function () {
+    const res = await fetch("https://picsum.photos/v2/list?limit=100");
+    /*parse the data captured in the res variable using the .json(). Hint: Don’t forget to use the await keyword*/
+    const images = await res.json();
+    console.log(images);
+};
+
+getImage();
