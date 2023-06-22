a0c7098f5a29b919e6d57eed0e72f07fbb715379
molly_b <brownmolly109@gmail.com>
2023-06-22 14:09:59 -0400

    revealing a random image

--- js/script.js
+++ js/script.js
@@ -7,7 +7,28 @@ const getImage = async function () {
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
     /*parse the data captured in the res variable using the .json(). Hint: Don’t forget to use the await keyword*/
     const images = await res.json();
-    console.log(images);
+    selectRandomImage(images);
 };
 
-getImage();
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    /*console.log(randomIndex);*/
+    const randomImage = images[randomIndex];
+    /***why isn't it randomIndex(images?***/
+   // console.log(randomImage);
+    displayImage(randomImage);
+};
+
+/***Display the Image***/
+const displayImage = function (randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+};
+
+//Add a Click Event
+button.addEventListener("click", function () {
+    getImage();
+});
